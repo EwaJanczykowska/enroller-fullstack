339bf13b21ab7619c4bb7d37085b9c9913c8efc4
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-05-28 17:40:37 +0200

    added id and date columns

--- src/main/frontend/src/meetings/MeetingsList.vue
+++ src/main/frontend/src/meetings/MeetingsList.vue
@@ -2,16 +2,20 @@
   <table v-if="meetings.length > 0">
     <thead>
     <tr>
+      <th>Id</th>
       <th>Nazwa spotkania</th>
       <th>Opis</th>
+      <th>Data</th>
       <th>Uczestnicy</th>
       <td></td>
     </tr>
     </thead>
     <tbody>
     <tr v-for="meeting in meetings" :key="meeting.name">
+      <td>{{ meeting.id }}</td>
       <td>{{ meeting.name }}</td>
       <td>{{ meeting.description }}</td>
+      <td>{{ meeting.date }}</td>
       <td>
         <ul v-if="meeting.participants">
           <li v-for="participant in meeting.participants" :key="participant.login">
